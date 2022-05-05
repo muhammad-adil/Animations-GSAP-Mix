3cc0e4580a1769c9e1ca26b5ca6a517198982b2f
Muhammad-Adil <adil.muhammad93@gmail.com>
2022-05-05 16:37:54 +0200

    gsap typing animation complete

---
+++ 02-Typing-Animation-GSAP/script.js
@@ -0,0 +1,36 @@
+const words = [
+  "a Student",
+  "a Front-End Web Developer",
+  "a passionate Photographer",
+  "a Chess lover"
+];
+
+let cursor = gsap.to(".cursor", {
+  opacity: 0,
+  ease: "power2.inOut",
+  repeat: -1
+});
+let masterTl = gsap.timeline({ repeat: -1 }).pause();
+let boxTl = gsap.timeline();
+
+boxTl
+  .to(".box", { duration: 1, width: "16vw", delay: 0.5, ease: "power4.inOut" })
+  .from(".hi", { duration: 1, y: "8vw", ease: "power3.out" })
+  .to(".box", {
+    duration: 1,
+    height: "7vw",
+    ease: "elastic.out",
+    onComplete: () => masterTl.play()
+  })
+  .to(".box", {
+    duration: 2,
+    autoAlpha: 0.7,
+    yoyo: true,
+    repeat: -1,
+    ease: "rough({ template: none.out, strength:  1, points: 20, taper: 'none', randomize: true, clamp: false})"
+  });
+words.forEach((word) => {
+  let tl = gsap.timeline({ repeat: 1, yoyo: true, repeatDelay: 1 });
+  tl.to(".text", { duration: 1, text: word });
+  masterTl.add(tl);
+});
