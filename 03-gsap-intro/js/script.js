238feeecd8e8221498558d7c035667c0617244dc
muhammad-adil <adil.muhammad93@gmail.com>
2023-11-11 12:12:00 +0100

    gsap.from() and stagger

--- 03-gsap-intro/js/script.js
+++ 03-gsap-intro/js/script.js
@@ -2,7 +2,9 @@ gsap.to("#box1", {
     x:1000,
     y:200,
     duration:3,
-    delay:1
+    delay:1,
+    backgroundColor: "blue",
+    borderRadius: "50%"
 })
 
 gsap.to("#box2", {
@@ -12,3 +14,12 @@ gsap.to("#box2", {
     delay:1
 })
 
+// gsap.from
+gsap.from(".main-heading", {
+    opacity: 0,
+    duration: 1,
+    delay: 1,
+    y: 50,
+    color: "red",
+    stagger: 0.5
+})
